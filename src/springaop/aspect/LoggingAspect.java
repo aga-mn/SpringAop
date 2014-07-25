24951d2b16a16e1b2172ef1cd5445e1aa9d561d0
aga-mn <molda.nieweglowska@gmail.com>
2014-07-25 21:35:28 +0200

    tutorial 28, pointcuts

--- src/springaop/aspect/LoggingAspect.java
+++ src/springaop/aspect/LoggingAspect.java
@@ -2,13 +2,29 @@ package springaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
+	//@Before("execution(public String get*())") for all public getters returning a string
+	//@Before("execution(public * get*())") for all public getters
+	//@Before("execution(public * get*(..))") for all public getters, any return type, any number of arguments 
+	//@Before("execution(public * springaop.model.*.get*())") all public getters from classes in package
+	
+	@Before("allGetters()")
 	public void LoggingAdvice(){
 		System.out.println("Advice run. Get Method called");
+	}
+	
+	@Before("allGetters()")
+	public void secondAdvice(){
+		System.out.println("Second Advice executed");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters(){
+		
 	}
 	
 }
