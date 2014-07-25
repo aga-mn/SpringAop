eb2ea4b37fb2b04cf6f33c5d93e2b2897f789199
aga-mn <molda.nieweglowska@gmail.com>
2014-07-25 20:51:02 +0200

    tutorial 27. creating aspects

---
+++ src/springaop/AopMain.java
@@ -0,0 +1,15 @@
+package springaop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springaop.service.ShapeService;
+
+public class AopMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("spring.xml");
+		ShapeService shapeService=ctx.getBean("shapeService", ShapeService.class);
+		System.out.println(shapeService.getTriangle().getName());
+	}
+
+}
